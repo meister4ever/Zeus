6a9c5500b7593c6f2062dea3d7ed31037de56e51
sebastiansavulsky <sasavulsky@gmail.com>
2015-09-13 23:35:07 -0300

    inicial

---
+++ sfcrime/src/sfcrime.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : sfcrime.cpp
+// Author      : Seba
+// Version     :
+// Copyright   : This was developed by intergalactic team.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl;
+	return 0;
+}
